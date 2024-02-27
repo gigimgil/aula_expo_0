47e67cfde96798dc7ebdd4b2cc660ed15e55469a
gigimgil <160647260+gigimgil@users.noreply.github.com>
2024-02-27 15:24:29 -0300

    aula27-02

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+
+function Exemplo0() {
+    return(
+        <View>
+            <Text>Minha primeira tela </Text>
+        </View>
+    );
+}
+export default Exemplo0;
