1ea7fcbdd8eedb9a67e5419956efc238ecfe72a8
gigimgil <160647260+gigimgil@users.noreply.github.com>
2024-05-03 09:45:39 -0300

    aula 3-05

--- components/exemplo_3/styles.js
+++ components/exemplo_3/styles.js
@@ -5,12 +5,22 @@ container: {
     flex:1,
     backgroundColor:'pink',
     borderRadius: 20,
+    width:'100%',
+    alignItems:'center',
 },
 titulo:{
     fontSize:30,
     fontWeight:'bold',
     marginVertical:20,
     color: 'violet',
-}
+},
+botao:{
+    borderWidth:3,
+    padding:10,
+    width:'80%',
+    borderRadius:20,
+    alignItems:center,
+},
+
 })
 export default styles;
