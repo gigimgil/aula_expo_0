daa52cf51666149ee359b5b471c1555cad25dde9
gigimgil <160647260+gigimgil@users.noreply.github.com>
2024-06-21 09:51:35 -0300

    aula 21/06

---
+++ components/exemplo_4/styles.js
@@ -0,0 +1,37 @@
+import{StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex:1,
+        backgroundColor:'pink',
+        borderRadius: 20,
+        width:'100%',
+        alignItems:'center',
+    },
+    titulo:{
+        fontSize:30,
+        fontWeight:'bold',
+        marginVertical:20,
+        color: 'violet',
+    },
+    input:{
+        fontSize:25,
+        fontWeight:'bold',
+        marginVertical:0,
+        color: 'violet',
+        width:'80%',
+        borderColor:'blue',
+        borderWidth:2,
+        padding:5,
+        borderRadius:20,
+    }
+    titulo:{
+        fontSize:30,
+        fontWeight:'bold',
+        marginVertical:20,
+        color: 'violet',
+    },
+
+});
+export default styles;
